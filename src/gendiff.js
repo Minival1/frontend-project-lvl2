665776d6abd7915c87855caa0a7e0b9f81f744e0
Valeriy <mrminival2@mail.ru>
2021-03-20 21:28:03 +0300

    fix

--- src/gendiff.js
+++ src/gendiff.js
@@ -1,18 +1,16 @@
 #!/usr/bin/env node
-import { Command } from 'commander';
+import commander from 'commander';
 import genDiff from './index.js';
 
-const program = new Command();
-
-program
+commander
 	.version('0.1')
 	.description('Compares two configuration files and shows a difference.')
 	.option('-f, --format [type]', 'output format', 'stylish')
 	.arguments('<filepath1> <filepath2>')
 	.action((filepath1, filepath2) => {
-		const options = program.opts();
+		const options = commander.opts();
 		const result = genDiff(filepath1, filepath2, options.format);
 		console.log(result);
 	});
 
-program.parse(process.argv);
+commander.parse(process.argv);
